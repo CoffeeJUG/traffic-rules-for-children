3369a2d963020a79248ca3b71967e6344d221655
Khrystyna Derenivska <khrisderenivska@gmail.com>
2020-08-29 09:58:22 +0300

    Component for puzzle page

---
+++ frontend/src/components/PuzzlePage/PuzzlePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+let PuzzlePage = () => {
+  return (
+    <div className="puzzle-page">
+      <div className="puzzle-page__img"></div>
+      <div className="puzzle-page__puzzle">
+        <div className="puzzle-page__choose"></div>
+      </div>
+    </div>
+  );
+};
+
+export default PuzzlePage;
