0ce691ec0e24953389067a8081a846c0183bc659
Volodymyr <vovastalker26@gmail.com>
2020-09-13 16:33:41 +0300

    create Map Profile Rank files

---
+++ frontend/src/components/pages/Map.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Map() {
+
+  return (
+    <h1>
+      Map
+    </h1>
+  );
+}
+
+export default Map;
---
+++ frontend/src/components/pages/Rank.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Rank() {
+
+  return (
+    <h1>
+      Rank
+    </h1>
+  );
+}
+
+export default Rank;
