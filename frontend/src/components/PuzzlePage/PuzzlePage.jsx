6e911e03ca77262ee0edec937a587709fc56aa65
Khrystyna Derenivska <khrisderenivska@gmail.com>
2020-09-15 09:10:55 +0300

    General page view

--- frontend/src/components/PuzzlePage/PuzzlePage.jsx
+++ frontend/src/components/PuzzlePage/PuzzlePage.jsx
@@ -4,8 +4,28 @@ import './PuzzlePage.css';
 let PuzzlePage = () => {
   return (
     <div className="puzzle-page">
-      <div className="puzzle-page__img"></div>
+      <div className="header">
+        <a className="header__link link" href="/">
+          Logo
+        </a>
+        <nav className="navigation">
+          <a className="navigation__link link" href="/">
+            Карта
+          </a>
+          <a className="navigation__link link" href="/">
+            Кращі гравці
+          </a>
+          <a className="navigation__link link" href="/">
+            Мій профіль
+          </a>
+        </nav>
+      </div>
+
       <div className="puzzle-page__puzzle">
+        <div className="puzzle-page__image">
+          <img src="/pictures/road-traffic-bg-img.png" alt="road-traffic"></img>
+        </div>
+
         <div className="puzzle-page__choose">
           <div className="puzzle-page__variant puzzle-variant">
             <img
@@ -18,10 +38,10 @@ let PuzzlePage = () => {
               <div className="puzzle__color puzzle__color--red"></div>
             </a>
           </div>
-          {/* <div className="puzzle-page__variant puzzle-variant">
+          <div className="puzzle-page__variant puzzle-variant">
             <img
-              src="../../images/images/pedestrian-lights.svg"
-              alt="pedestrian-lights"
+              src="pictures/car-lights.svg"
+              alt="car-lights"
               className="puzzle-variant__image"
             />
             <a href="/" className="puzzle-variant__puzzle puzzle">
@@ -29,7 +49,10 @@ let PuzzlePage = () => {
               <div className="puzzle__color puzzle__color--yellow"></div>
               <div className="puzzle__color puzzle__color--red"></div>
             </a>
-          </div> */}
+          </div>
+          <a href="/" className="puzzle-page__button">
+            Готово
+          </a>
         </div>
       </div>
     </div>
