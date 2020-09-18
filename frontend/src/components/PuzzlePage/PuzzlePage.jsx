427efcf706927fd0c4aabfd04c57bab05d0ef4d1
Khrystyna Derenivska <khrisderenivska@gmail.com>
2020-09-19 02:03:31 +0300

    Created draggable items

--- frontend/src/components/PuzzlePage/PuzzlePage.jsx
+++ frontend/src/components/PuzzlePage/PuzzlePage.jsx
@@ -1,66 +1,99 @@
 import React from 'react';
+import { Component } from 'react';
 import './PuzzlePage.css';
+import $ from 'jquery';
 
-let PuzzlePage = () => {
-  return (
-    <div className="puzzle-page">
-      <div className="header">
-        <a className="header__link link" href="/">
-          Logo
-        </a>
-        <nav className="navigation">
-          <a className="navigation__link link" href="/">
-            Карта
-          </a>
-          <a className="navigation__link link" href="/">
-            Кращі гравці
-          </a>
-          <a className="navigation__link link" href="/">
-            Мій профіль
-          </a>
-        </nav>
-      </div>
+class PuzzlePage extends Component {
+  componentDidMount() {}
 
-      <div className="puzzle-page__puzzle">
-        <div className="puzzle-page__image">
-          <img src="/pictures/road-traffic-bg-img.png" alt="road-traffic"></img>
-
-          <div className="puzzle-page__instruction">
-            Встав кольори в правильному порядку.
-          </div>
+  render() {
+    return (
+      <div className="puzzle-page">
+        <div className="header">
+          <a className="header__link link" href="/">
+            Logo
+          </a>
+          <nav className="navigation">
+            <a className="navigation__link link" href="/">
+              Карта
+            </a>
+            <a className="navigation__link link" href="/">
+              Кращі гравці
+            </a>
+            <a className="navigation__link link" href="/">
+              Мій профіль
+            </a>
+          </nav>
         </div>
 
-        <div className="puzzle-page__choose">
-          <div className="puzzle-page__variant puzzle-variant">
+        <div className="puzzle-page__puzzle">
+          <div className="puzzle-page__image">
             <img
-              src="pictures/pedestrian-lights.png"
-              alt="pedestrian-lights"
-              className="puzzle-variant__image"
-            />
-            <a href="/" className="puzzle-variant__puzzle puzzle">
-              <div className="puzzle__color puzzle__color--green"></div>
-              <div className="puzzle__color puzzle__color--red"></div>
+              src="/pictures/road-traffic-bg-img.png"
+              alt="road-traffic"
+            ></img>
+
+            <div className="puzzle-page__instruction">
+              Встав кольори в правильному порядку.
+            </div>
+          </div>
+
+          <div className="puzzle-page__choose">
+            <div className="puzzle-page__variant puzzle-variant">
+              <div className="puzzle-variant__image">
+                <img
+                  src="pictures/pedestrian-lights.png"
+                  alt="pedestrian-lights"
+                />
+                <div className="empty empty--top"></div>
+                <div className="empty empty--bottom"></div>
+              </div>
+
+              <div className="puzzle-variant__puzzle puzzle">
+                <div
+                  className="puzzle__color puzzle__color--green draggable"
+                  draggable="true"
+                ></div>
+                <div
+                  className="puzzle__color puzzle__color--red draggable"
+                  draggable="true"
+                ></div>
+              </div>
+            </div>
+            <div className="puzzle-page__variant puzzle-variant">
+              <div className="puzzle-variant__image car-light__image">
+                <img
+                  src="pictures/car-lights.svg"
+                  alt="car-lights"
+                  className="puzzle-variant__image"
+                />
+                <div className="empty sec_empty--top"></div>
+                <div className="empty sec_empty--middle"></div>
+                <div className="empty sec_empty--bottom"></div>
+              </div>
+              <div className="puzzle-variant__puzzle puzzle">
+                <div
+                  className="puzzle__color puzzle__color--green draggable"
+                  draggable="true"
+                ></div>
+                <div
+                  className="puzzle__color puzzle__color--yellow draggable"
+                  draggable="true"
+                ></div>
+                <div
+                  className="puzzle__color puzzle__color--red draggable"
+                  draggable="true"
+                ></div>
+              </div>
+            </div>
+            <a href="/" className="puzzle-page__button">
+              Готово
             </a>
           </div>
-          <div className="puzzle-page__variant puzzle-variant">
-            <img
-              src="pictures/car-lights.svg"
-              alt="car-lights"
-              className="puzzle-variant__image"
-            />
-            <a href="/" className="puzzle-variant__puzzle puzzle">
-              <div className="puzzle__color puzzle__color--green"></div>
-              <div className="puzzle__color puzzle__color--yellow"></div>
-              <div className="puzzle__color puzzle__color--red"></div>
-            </a>
-          </div>
-          <a href="/" className="puzzle-page__button">
-            Готово
-          </a>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default PuzzlePage;
