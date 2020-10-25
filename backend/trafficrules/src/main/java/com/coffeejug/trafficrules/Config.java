89396a9825eca2840cb606f6980abfa817a412b7
sergiykatriuk <sergiy.katriuk@gmail.com>
2020-10-25 09:46:19 +0200

    Transferred RuntimeMXBean to Bean.

--- backend/trafficrules/src/main/java/com/coffeejug/trafficrules/Config.java
+++ backend/trafficrules/src/main/java/com/coffeejug/trafficrules/Config.java
@@ -4,11 +4,19 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
 @Configuration
 public class Config {
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public RuntimeMXBean runtimeMxBean() {
+        return ManagementFactory.getRuntimeMXBean();
     }
 }
--- backend/trafficrules/src/main/java/com/coffeejug/trafficrules/service/StatisticService.java
+++ backend/trafficrules/src/main/java/com/coffeejug/trafficrules/service/StatisticService.java
@@ -1,13 +1,17 @@
 package com.coffeejug.trafficrules.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 
 @Service
 public class StatisticService {
 
+    @Autowired
+    private RuntimeMXBean runtimeMXBean;
+
     public long getUptime() {
-        return ManagementFactory.getRuntimeMXBean().getUptime();
+        return runtimeMXBean.getUptime();
     }
 }
