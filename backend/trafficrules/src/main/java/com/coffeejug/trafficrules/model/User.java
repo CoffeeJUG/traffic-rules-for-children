690495db61167ff82b1219efe4708e7504af3fc2
sergiykatriuk <sergiy.katriuk@gmail.com>
2020-11-02 09:42:18 +0200

    Removed @columnDefinition.

--- backend/trafficrules/src/main/java/com/coffeejug/trafficrules/model/User.java
+++ backend/trafficrules/src/main/java/com/coffeejug/trafficrules/model/User.java
@@ -18,9 +18,7 @@ public class User {
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(columnDefinition = "BINARY(16)")
     private UUID uuid;
-    @Column(columnDefinition = "INT NOT NULL DEFAULT '0'")
     private int levelsCompleted;
     private String name;
 
