4f64cb55a61d5540a4ac03a7c3421a6aecca9e1a
Serge Bishyr <seriybg@gmail.com>
2020-11-08 09:36:17 +0200

    add example of test with argument captor

--- backend/trafficrules/src/test/java/com/coffeejug/trafficrules/service/StatisticServiceTest.java
+++ backend/trafficrules/src/test/java/com/coffeejug/trafficrules/service/StatisticServiceTest.java
@@ -1,8 +1,13 @@
 package com.coffeejug.trafficrules.service;
 
 import java.lang.management.RuntimeMXBean;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -10,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,6 +28,9 @@ class StatisticServiceTest {
 
     @InjectMocks
     private StatisticService statisticService;
+
+    @Captor
+    private ArgumentCaptor<LocalDateTime> localDateTimeArgumentCaptor;
 
     @Test
     void shouldReturnCorrectUptime() {
@@ -42,5 +51,20 @@ class StatisticServiceTest {
         assertThatThrownBy(() -> statisticService.uptime())
                 .isInstanceOf(RuntimeException.class)
                 .hasMessage("Something goes wrong");
+    }
+
+    @Test
+    void shouldReturnAllUsersRegisteredFromStart() {
+        when(runtimeMXBean.getUptime()).thenReturn(1000L);
+        when(userService.countAllByRegisteredAfter(any())).thenReturn(15L);
+
+        LocalDateTime beforeServiceCall = LocalDateTime.now();
+
+        assertThat(statisticService.usersRegisteredFromStart()).isEqualTo(15L);
+
+        verify(userService).countAllByRegisteredAfter(localDateTimeArgumentCaptor.capture());
+        LocalDateTime calledValue = localDateTimeArgumentCaptor.getValue();
+        assertThat(calledValue).isBefore(LocalDateTime.now());
+        assertThat(calledValue).isAfter(beforeServiceCall.minus(1000, ChronoUnit.MILLIS));
     }
 }
