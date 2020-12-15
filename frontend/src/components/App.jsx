65c8db488da5cbd834c88bb85b253ac045d17eb6
DrabykMykola <drabyk999@ukr.net>
2020-12-15 19:43:08 +0200

    add transition from map to quests

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './Navbar/Navar';
-// import Quest from './pages/QuestPage/Quest';
-// import PuzzlePage from './PuzzlePage/PuzzlePage';
-// import CanvasPage from './pages/Canvas/CanvasPage';
 import Map from './pages/Map/Map';
+import Pazzle from "./pages/Pazzle/Pazzle";
+import BestPlayers from "./pages/BestPlayers/BestPlayers";
+import Profile from "./pages/Profile/Profile";
+import Pazzle1 from "./pages/Pazzle1/Pazzle1";
+import MainPage from "./pages/MainPage/MainPage";
+
+
+
+
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Map />
-    </div>
+      <BrowserRouter>
+        <div>
+            <Navbar />
+            <Route path='/mainPage' component={MainPage}/>
+              <Route path='/map' component={Map}/>
+              <Route path='/pazzle' component={Pazzle}/>
+              <Route path='/pazzle-1' component={Pazzle1}/>
+              <Route path='/bestPlayers' component={BestPlayers}/>
+              <Route path='/profile' component={Profile}/>
+        </div>
+      </BrowserRouter>
   );
 }
 
--- frontend/src/components/pages/Map/Map.jsx
+++ frontend/src/components/pages/Map/Map.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import  '../Canvas/Canvas';
 import './Map.css';
 import ModalForm from '../../Modal';
 import { get } from 'lodash';
 import { quests } from '../../../constants';
 import bg from '../../../images/bg.jpg';
+import marker1 from '../../../images/marker1.png';
+import marker2 from '../../../images/marker2.png';
+
+
 function Map() {
   const [correct, setCorrect] = useState(true);
   const [modal, setModal] = useState(false);
@@ -14,9 +19,16 @@ function Map() {
     setModal(!modal);
   };
 
+
+
   return (
+      <div>
       <div className="quest-main__map">
           <img src={bg} alt="..." className="quest-main-img" />
+          <a href="/pazzle"><img src={marker1} alt="..." className="quest-marker1-img" /></a>
+          <a href="/pazzle-1"><img src={marker2} alt="..." className="quest-marker2-img" /></a>
+      </div>
+    <button id="toggleFullScreen" data-toggle-fullscreen>Toggle Fullscreen</button>
       </div>
   );
 }
