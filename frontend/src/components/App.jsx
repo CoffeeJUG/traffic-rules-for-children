c7335b5b8de7165d4402b80f43da393672a0bdd4
Eugene <medium4uk@gmail.com>
2020-12-23 20:48:27 +0200

    added cookie

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -6,6 +6,17 @@ import Navbar from './Navbar/Navar';
 // import CanvasPage from './pages/Canvas/CanvasPage';
 import Map from './pages/Map/Map';
 
+import Cookies from 'js-cookie'
+
+Cookies.set('puzzle-kid-game', 
+	{
+	"name" : "Irusya",
+	"completedLevel" : 5,
+	"mistakes" : 3,
+	}
+);
+console.log(Cookies.get('puzzle-kid-game'));
+
 function App() {
   return (
     <div>
