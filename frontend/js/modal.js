5a4af5fbc2158e990b9266c07ddbe743ceb8b48e
DrabykMykola <drabyk999@ukr.net>
2021-01-18 20:22:52 +0200

    add modal window+

--- frontend/js/modal.js
+++ frontend/js/modal.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-	$('a.login-window').click(function() {
+	// $('a.login-window').click(function() {
 		
 		// Getting the variable's value from a link 
 		var loginBox = $(this).attr('href');
 
 		//Fade in the Popup and add close button
-		$(loginBox).fadeIn(300);
+		// $(loginBox).fadeIn(300);
 		
 		//Set the center alignment padding + border
 		var popMargTop = ($(loginBox).height() + 24) / 2; 
@@ -17,17 +17,17 @@ $(document).ready(function() {
 		});
 		
 		// Add the mask to body
-		$('body').append('<div id="mask"></div>');
-		$('#mask').fadeIn(300);
+		// $('body').append('<div id="mask"></div>');
+		// $('#mask').fadeIn(300);
 		
-		return false;
-	});
+		// return false;
+	// });
 	
 	// When clicking on the button close or the mask layer the popup closed
-	$('a.close, #mask').live('click', function() { 
-	  $('#mask , .login-popup').fadeOut(300 , function() {
-		$('#mask').remove();  
-	}); 
-	return false;
-	});
+	// $('a.close, #mask').live('click', function() { 
+	//   $('#mask , .login-modal').fadeOut(300 , function() {
+	// 	$('#mask').remove();  
+	// }); 
+	// return false;
+	// });
 });
